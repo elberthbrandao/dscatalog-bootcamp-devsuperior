c9cc09a26361bed1da987c154cd5a1f7c98154b1
elberthbrandao <zelltr@hotmail.com>
2021-04-07 17:40:36 -0300

    Implementing OAuth2 authorization server

--- backend/src/main/java/com/elberthbrandao/dscatalog/entities/User.java
+++ backend/src/main/java/com/elberthbrandao/dscatalog/entities/User.java
@@ -1,8 +1,10 @@
 package com.elberthbrandao.dscatalog.entities;
 
 import java.io.Serializable;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -14,11 +16,14 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
-import javax.validation.constraints.NotEmpty;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 @Entity
 @Table(name = "tb_user")
-public class User implements Serializable{
+public class User implements UserDetails, Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	@Id
@@ -30,7 +35,6 @@ public class User implements Serializable{
 	@Column(unique = true)
 	private String email;
 	private String password;
-	
 	
 	@ManyToMany(fetch = FetchType.EAGER)
 	@JoinTable(name = "tb_user_role",
@@ -147,6 +151,37 @@ public class User implements Serializable{
 				return false;
 		} else if (!password.equals(other.password))
 			return false;
+		return true;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return roles.stream().map(role -> new SimpleGrantedAuthority(role.getAuthority()))
+				.collect(Collectors.toList());
+	}
+
+	@Override
+	public String getUsername() {
+		return email;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
 		return true;
 	}
 	
